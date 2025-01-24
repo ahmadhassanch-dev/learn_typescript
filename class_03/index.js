a3fb2db960f935909b36a1021056615697f57adf
ahmadhassanch-dev <ahmadhassanch.dev@gmail.com>
2025-01-24 19:03:24 +0500

    3rd class

--- class_03/index.js
+++ class_03/index.js
@@ -1,28 +1,91 @@
 "use strict";
-let age = 59;
-if (age < 0) {
-    console.log("Age cannot be negative.");
-}
-else if (age >= 0 && age <= 12) {
-    console.log("You are a child.");
-}
-else if (age >= 13 && age <= 19) {
-    console.log("You are a teenager.");
-}
-else if (age >= 20 && age <= 35) {
-    console.log("You are a young adult.");
-}
-else if (age >= 36 && age <= 60) {
-    if (age == 60) {
-        console.log("You are a senior citizen.");
-    }
-    else if (age >= 36 && age < 60) {
-        console.log("You are an adult.");
-    }
-}
-else if (age > 60) {
-    console.log("You are a senior citizen.");
-}
-else {
-    console.log("Invalid age.");
-}
+// let age : number = 40
+// if(age <= 0){
+//         console.log("Age cannot be negative.");
+//     }
+// else if (age >= 1 && age <= 12){
+//     console.log("You are a child.");
+// }
+// else if (age >= 30 && age <= 60){
+//     if (age ==60){
+//         console.log("You are a senior citizen.");
+//     }else if (age >= 40 && age <= 50){
+//             console.log("You are a middle aged adult.");
+//         }else {
+//         console.log("You Are A young person.");
+//     }
+// }
+// else {
+//     console.log("You are an adult.");
+// }
+// if(age <= 0){
+//         console.log("Age cannot be negative.");
+//     }
+//     else if (age >= 1 && age <= 12){
+//             console.log("You are a child.");
+// }
+// else if (age >= 30 && age <= 60){
+//         switch (true){
+//                 case age >= 40 && age <= 50:
+//                     console.log("You are a middle aged adult.");
+//                     break;
+//         case age >= 51 && age <= 60:
+//             console.log("You are a senior citizen.");
+//             break;
+//         default:
+//             console.log("You are an adult.");
+//             break;
+//     }
+// }
+// else {
+//         console.log("I dont know who you are.");
+//     }
+// switch (true) {
+//     case age <= 0:
+//         console.log("Age cannot be negative.");
+//         break;
+//     case age >= 1 && age <= 12:
+//         console.log("You are a child.");
+//         break;
+//     case age >= 30 && age <= 60:
+//        switch (true){
+//         case age >= 40 && age <= 50:
+//             console.log("You are a middle aged adult.");
+//             break;
+//         case age >= 51 && age <= 60:
+//             console.log("You are a senior citizen.");
+//             break;
+//         default:
+//             console.log("You are an adult.");
+//             break;
+//        }
+//    break;
+//     default:
+//         console.log("You are an adult.");
+//         break;
+// }
+// switch (true) {
+//     case age <= 0:
+//         console.log("Age cannot be negative.");
+//         break;
+//     case age >= 1 && age <= 12:
+//         console.log("You are a child.");
+//         break;
+//     case age >= 30 && age <= 60:
+//        switch (true){
+//         case age >= 40 && age <= 50:
+//             console.log("You are a middle aged adult.");
+//             break;
+//         case age >= 51 && age <= 60:
+//             console.log("You are a senior citizen.");
+//             break;
+//         default:
+//             console.log("You are an adult.");
+//             break;
+//        }
+//    break;
+//     default:
+//         console.log("You are an adult.");
+//         break;
+// }
+console.log("Hello \n \t World");
--- class_03/index.ts
+++ class_03/index.ts
@@ -1,71 +1,108 @@
-let age: number = 59;
+// let age : number = 40
 
-if (age < 0) {
-    console.log("Age cannot be negative.");
-} else if (age >= 0 && age <= 12) {
-    console.log("You are a child.");
-} else if (age >= 13 && age <= 19) {
-    console.log("You are a teenager.");
-} else if (age >= 20 && age <= 35) {
-    console.log("You are a young adult.");
-} else if (age >= 36 && age <= 60) {
-    if(age == 60 ){
-        console.log("You are a senior citizen.");
-    }else if (age >=36 && age < 60){
-    console.log("You are an adult.");}
-} else if (age > 60) {
-    console.log("You are a senior citizen.");
-} else {
-    console.log("Invalid age.");
-}
-
-
-
-
-// let age: number = 18;
-
-// switch (age) {
-//     case 10:
-//         console.log("You are a child.");
-//         break;
-//     case 18:
-//         console.log("You are a teenager.");
-//         break;
-//     case 25:
-//         console.log("You are a young adult.");
-//         break;
-//     case 40:
-//         console.log("You are an adult.");
-//         break;
-//     case 65:
+// if(age <= 0){
+//         console.log("Age cannot be negative.");
+//     }
+// else if (age >= 1 && age <= 12){
+//     console.log("You are a child.");
+// }
+// else if (age >= 30 && age <= 60){
+//     if (age ==60){
 //         console.log("You are a senior citizen.");
-//         break;
-//     default:
-//         console.log("Age group not specifically defined.");
+//     }else if (age >= 40 && age <= 50){
+//             console.log("You are a middle aged adult.");
+//         }else {
+//         console.log("You Are A young person.");
+//     }
+// }
+// else {
+//     console.log("You are an adult.");
 // }
 
 
-// let age: number = 18;
+
+
+// if(age <= 0){
+//         console.log("Age cannot be negative.");
+//     }
+//     else if (age >= 1 && age <= 12){
+//             console.log("You are a child.");
+// }
+// else if (age >= 30 && age <= 60){
+//         switch (true){
+//                 case age >= 40 && age <= 50:
+//                     console.log("You are a middle aged adult.");
+//                     break;
+//         case age >= 51 && age <= 60:
+//             console.log("You are a senior citizen.");
+//             break;
+
+//         default:
+//             console.log("You are an adult.");
+//             break;
+//     }
+// }
+// else {
+//         console.log("I dont know who you are.");
+//     }
+    
+    
+
 
 // switch (true) {
-//     case age < 0:
+//     case age <= 0:
 //         console.log("Age cannot be negative.");
 //         break;
-//     case age >= 0 && age <= 12:
+//     case age >= 1 && age <= 12:
 //         console.log("You are a child.");
 //         break;
-//     case age >= 13 && age <= 19:
-//         console.log("You are a teenager.");
-//         break;
-//     case age >= 20 && age <= 35:
-//         console.log("You are a young adult.");
-//         break;
-//     case age >= 36 && age <= 60:
+//     case age >= 30 && age <= 60:
+//        switch (true){
+//         case age >= 40 && age <= 50:
+//             console.log("You are a middle aged adult.");
+//             break;
+//         case age >= 51 && age <= 60:
+//             console.log("You are a senior citizen.");
+//             break;
+//         default:
+//             console.log("You are an adult.");
+//             break;
+//        }
+//    break;
+//     default:
 //         console.log("You are an adult.");
 //         break;
-//     case age > 60:
-//         console.log("You are a senior citizen.");
-//         break;
-//     default:
-//         console.log("Invalid age.");
 // }
+
+
+
+
+
+
+// switch (true) {
+//     case age <= 0:
+//         console.log("Age cannot be negative.");
+//         break;
+//     case age >= 1 && age <= 12:
+//         console.log("You are a child.");
+//         break;
+//     case age >= 30 && age <= 60:
+//        switch (true){
+//         case age >= 40 && age <= 50:
+//             console.log("You are a middle aged adult.");
+//             break;
+//         case age >= 51 && age <= 60:
+//             console.log("You are a senior citizen.");
+//             break;
+//         default:
+//             console.log("You are an adult.");
+//             break;
+//        }
+//    break;
+//     default:
+//         console.log("You are an adult.");
+//         break;
+// }
+
+
+
