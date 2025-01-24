6b74a4e0f366b135ed26c68b0a2cee7beca835b3
ahmadhassanch-dev <ahmadhassanch.dev@gmail.com>
2025-01-24 19:09:43 +0500

    4th class

---
+++ class_04/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// make a normal sum function
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(5, 4));
---
+++ class_04/index.ts
@@ -0,0 +1,12 @@
+
+function sum(a: number, b: number): number {
+    return a + b;
+}
+console.log(sum(5,4))
+
+
+
+const sumArrow = (a: number, b: number): number => {
+    return a + b;
+}
+console.log(sumArrow(5,4))
